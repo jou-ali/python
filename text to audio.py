56fc3b51de0b4ba95081f7b69b99db8a1a05c899
MUHAMMAD JASEEL C K <78477250+Jaseelmhmd@users.noreply.github.com>
2022-10-04 19:57:21 +0530

    Update text to audio.py

--- text to audio.py
+++ text to audio.py
@@ -1,6 +1,6 @@
 from gtts import gTTS
 from playsound import playsound
-text=input("Enter a sentence : ")
+text=input("Enter a sentence (In English) : ") #Taking user input
 speech=gTTS(text=text,lang="en")        #converting to mp3 in English
 speech.save(r"C:\Users\jouha\Desktop\jou.mp3")        #saving the .mp3 file in the given directory
 playsound(r"C:\Users\jouha\Desktop\jou.mp3")          
